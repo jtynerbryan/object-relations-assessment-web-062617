fe9fd6bbc32652f5a02d9c8a91758b5ffb71ff71
Jesse Tyner-Bryan <jesse@Jesses-MacBook-Air.local>
2017-07-10 10:55:34 -0400

    object relations assessment complete

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,43 +1,43 @@
 class Customer
-  attr_accessor :first_name, :last_name, :reviews
+	attr_accessor :first_name, :last_name, :reviews
 
-  @@all = []
+	@@all = []
 
-  def initialize(first_name, last_name)
-    @first_name = first_name
-    @last_name  = last_name
-    @@all << self
-    @reviews = []
-  end
+	def initialize(first_name, last_name)
+	@first_name = first_name
+	@last_name  = last_name
+	@@all << self
+	@reviews = []
+	end
 
-  def full_name
-    "#{first_name} #{last_name}"
-  end
+	def full_name
+	"#{first_name} #{last_name}"
+	end
 
-  def self.all
-  	@@all
-  end
+	def self.all
+		@@all
+	end
 
-  def self.find_by_name(name)
-  	@@all.detect {|customer| name == customer.full_name}
-  end
+	def self.find_by_name(name)
+		@@all.detect {|customer| name == customer.full_name}
+	end
 
-  def self.find_all_by_first_name(first_name)
-  	@@all.select {|customer| customer.first_name}
-  end
+	def self.find_all_by_first_name(first_name)
+		@@all.select {|customer| customer.first_name}
+	end
 
-  def self.all_names
-  	@@all.map {|customer| customer.full_name}
-  end
+	def self.all_names
+		@@all.map {|customer| customer.full_name}
+	end
 
-  def add_review(restaurant, content)
-  	review = Review.new(restaurant, content)
-  	restaurant.customers << self
-  	review.customer = self
-  	@reviews << review
-  end
+	def add_review(restaurant, content)
+		review = Review.new(restaurant, content)
+		restaurant.customers << self
+		review.customer = self
+		@reviews << review
+	end
 
-  def reviews
-  	@reviews
-  end
+	def reviews
+		@reviews
+	end
 end
--- solution.rb
+++ solution.rb
@@ -1,46 +1,46 @@
 # Please copy/paste all three classes into this file to submit your solution!
 class Customer
-  attr_accessor :first_name, :last_name, :reviews
+	attr_accessor :first_name, :last_name, :reviews
 
-  @@all = []
+	@@all = []
 
-  def initialize(first_name, last_name)
-    @first_name = first_name
-    @last_name  = last_name
-    @@all << self
-    @reviews = []
-  end
+	def initialize(first_name, last_name)
+	@first_name = first_name
+	@last_name  = last_name
+	@@all << self
+	@reviews = []
+	end
 
-  def full_name
-    "#{first_name} #{last_name}"
-  end
+	def full_name
+	"#{first_name} #{last_name}"
+	end
 
-  def self.all
-  	@@all
-  end
+	def self.all
+		@@all
+	end
 
-  def self.find_by_name(name)
-  	@@all.detect {|customer| name == customer.full_name}
-  end
+	def self.find_by_name(name)
+		@@all.detect {|customer| name == customer.full_name}
+	end
 
-  def self.find_all_by_first_name(first_name)
-  	@@all.select {|customer| customer.first_name}
-  end
+	def self.find_all_by_first_name(first_name)
+		@@all.select {|customer| customer.first_name}
+	end
 
-  def self.all_names
-  	@@all.map {|customer| customer.full_name}
-  end
+	def self.all_names
+		@@all.map {|customer| customer.full_name}
+	end
 
-  def add_review(restaurant, content)
-  	review = Review.new(restaurant, content)
-  	restaurant.customers << self
-  	review.customer = self
-  	@reviews << review
-  end
+	def add_review(restaurant, content)
+		review = Review.new(restaurant, content)
+		restaurant.customers << self
+		review.customer = self
+		@reviews << review
+	end
 
-  def reviews
-  	@reviews
-  end
+	def reviews
+		@reviews
+	end
 end
 
 class Restaurant
