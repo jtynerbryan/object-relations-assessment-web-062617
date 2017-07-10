f4c9cb7afa149fb94bc278da4a76b1ce7c0551a6
Jesse Tyner-Bryan <jesse@Jesses-MacBook-Air.local>
2017-07-10 10:53:47 -0400

    object relations assessment complete

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,28 @@
 class Restaurant
-  attr_accessor :name
+  attr_accessor :name, :reviews, :customers
+
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
+    @customers = []
+    @reviews = []
   end
 
+  def self.all
+  	@@all
+  end
+
+  def self.find_by_name(name)
+  	@@all.detect {|restaurant| name == restaurant.name}
+  end
+
+  def reviews
+  	@reviews
+  end
+
+  def customers
+  	@customers
+  end
 end
